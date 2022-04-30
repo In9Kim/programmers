07042d5e6fc16e0651e10f05d00701c2aa827d42
In9Kim <wwe9459@naver.com>
2022-04-30 11:32:42 +0900

    20220430;kik;프로그래머스 1단계 반영

---
+++ src/kakao_blind_2022_1/Solution_main.java
@@ -0,0 +1,8 @@
+package kakao_blind_2022_1;
+
+public class Solution_main {
+    public static void main(String[]args){
+        Solution s = new Solution();
+        s.solution("...!@BaT#*..y.abcdefghijklm");
+    }
+}
